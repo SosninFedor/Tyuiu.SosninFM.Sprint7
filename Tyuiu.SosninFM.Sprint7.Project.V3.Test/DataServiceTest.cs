529209171091f03469206629853679d4e21bbdcb
SosninFedor <sosninfedor@gmail.com>
2024-12-17 18:30:12 +0500

    Project

---
+++ Tyuiu.SosninFM.Sprint7.Project.V3.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SosninFM.Sprint7.Project.V3.Test
+{
+	[TestClass]
+	public sealed class DataServiceTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ Tyuiu.SosninFM.Sprint7.Project.V3/FormMain.cs
@@ -0,0 +1,18 @@
+namespace Tyuiu.SosninFM.Sprint7.Project.V3
+{
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+
+
+
+		}
+	}
+}
