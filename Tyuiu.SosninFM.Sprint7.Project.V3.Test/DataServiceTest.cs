0d1f4a9a8f548e94922240298badd1563a01b791
SosninFedor <sosninfedor@gmail.com>
2024-12-25 18:23:39 +0500

    Project

--- Tyuiu.SosninFM.Sprint7.Project.V3.Test/DataServiceTest.cs
+++ Tyuiu.SosninFM.Sprint7.Project.V3.Test/DataServiceTest.cs
@@ -1,11 +1,20 @@
-﻿namespace Tyuiu.SosninFM.Sprint7.Project.V3.Test
+﻿using Tyuiu.SosninFM.Sprint7.Project.V3.Lib;
+using System.IO;
+
+namespace Tyuiu.SosninFM.Sprint7.Project.V3.Test
 {
 	[TestClass]
-	public sealed class DataServiceTest
+	public class DataServiceTest
 	{
 		[TestMethod]
 		public void TestMethod1()
 		{
+			DataService ds = new DataService();
+
+			string path = @"D:\Programs\VisualStudio\Projects\repos\2024\Tyuiu.SosninFM.Sprint7\Info.csv";
+			FileInfo fileInfo = new FileInfo(path);
+			bool fileExist = fileInfo.Exists;
+			Assert.AreEqual(true, fileExist);
 		}
 	}
 }
--- Tyuiu.SosninFM.Sprint7.Project.V3/FormAbout.Designer.cs
+++ Tyuiu.SosninFM.Sprint7.Project.V3/FormAbout.Designer.cs
@@ -28,10 +28,16 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "FormAbout";
+			SuspendLayout();
+			// 
+			// FormAbout
+			// 
+			AutoScaleDimensions = new SizeF(13F, 32F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1799, 1070);
+			Name = "FormAbout";
+			Text = "FormAbout";
+			ResumeLayout(false);
 		}
 
 		#endregion
